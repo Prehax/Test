46237ec95f0d52f349bb5bdba9e88e456157b36b
Longlong <lxu3@unh.newhaven.edu>
2020-02-27 17:47:19 -0500

    First Commit

--- app/src/main/java/com/prehax/test/MainActivity.java
+++ app/src/main/java/com/prehax/test/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //cao
+    // 这是我今天添加的东西， 2 月 27 日
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
