fa7a108e7a3f4af6e6db6228dd6ace5750e0b9c6
cheng <cheng530892953@gmail.com>
2020-02-27 17:50:16 -0500

    qsweqwe1

--- app/src/main/java/com/prehax/test/MainActivity.java
+++ app/src/main/java/com/prehax/test/MainActivity.java
@@ -5,8 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//cao
-    // 这是我今天添加的东西， 2 月 27 日
+//cao 123123
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
